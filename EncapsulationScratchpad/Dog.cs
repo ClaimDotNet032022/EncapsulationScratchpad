ad1b5ccf4216c941ca8c3e7f082f0a093aa27927
Eric Ray <erichamion@gmail.com>
2022-04-21 19:33:52 -0500

    Add project files.

---
+++ EncapsulationScratchpad/Dog.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationScratchpad
+{
+    public class Dog
+    {
+        public bool IsSitting { get; private set; } = true;
+        //public bool IsRunning { get; private set; }
+        public string Name { get; }
+        public bool IsHungry { get; private set; }
+
+        private bool _isRunning;
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+
+            set
+            {
+                _isRunning = value;
+                if (value)
+                {
+                    IsSitting = false;
+                }
+            }
+        }
+
+
+        public Dog(string name)
+        {
+            Name = name;
+            IsHungry = true;
+        }
+
+        public void Sit()
+        {
+            IsSitting = true;
+            IsRunning = false;
+        }
+
+        public void Stand()
+        {
+            IsSitting = false;
+            IsRunning = false;
+        }
+
+        public void Run()
+        {
+            IsSitting = false;
+            IsRunning = true;
+        }
+
+        public void Eat()
+        {
+            if (IsHungry)
+            {
+                Console.WriteLine("Yum");
+            }
+            IsHungry = false;
+        }
+
+        public void PassTime()
+        {
+            this.IsHungry = true;
+        }
+    }
+}
---
+++ EncapsulationScratchpad/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EncapsulationScratchpad;
+
+Console.WriteLine("Hello, World!");
+
+
+Dog rex = new Dog("Rex");
+Dog spot = new Dog("Spot");
+
+rex.Sit();
+//rex.Run();
+rex.IsRunning = true;
+rex.PassTime();
+rex.Eat();
+
+Console.WriteLine("Done");
